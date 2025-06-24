f3e734b5f3a5655465eb3f221349d19bf37dbb45
Matheus Zoccoler <mtzcooler@gmail.com>
2025-06-24 18:20:49 +0100

    Adding query parameters

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
+
 @app.get("/")
-def index():
-    return {"message": "Hello World"}
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {"data": f"{limit} published blogs from the database."}
+    else:
+        return {"data": f"{limit} blogs from the database."}
 
-@app.get('/about')
-def about():
-    return {"data": "about page"}
 
+@app.get('/blog/unpublished')
+def unpublished():
+    return {"data": "All unpublished blogs"}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {"data": id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    return {"data": {'Comment A', 'Comment B'}}
