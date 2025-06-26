c32968b8ff49958c579b123fc290684c2e47eb33
Matheus Zoccoler <mtzcooler@gmail.com>
2025-06-26 15:46:27 +0100

    Add index and show operations

--- blog/main.py
+++ blog/main.py
@@ -1,21 +1,13 @@
 from fastapi import FastAPI, Depends
 from dto.blogdto import BlogDTO
 from domain.blog import Blog
-from database import engine, Base, SessionLocal
+from database import engine, Base, get_db
 from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 
 Base.metadata.create_all(bind=engine)
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 @app.post('/blog')
@@ -26,3 +18,14 @@ def create(request: BlogDTO, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+
+@app.get('/blog')
+def index(db: Session = Depends(get_db)):
+    blogs = db.query(Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}')
+def show(id: int, db: Session = Depends(get_db)):
+    blog = db.query(Blog).filter(Blog.id == id).first()
+    return blog
