6102efcb8238bf59a8f64dabf5e402d410ae1412
Matheus Zoccoler <mtzcooler@gmail.com>
2025-06-29 18:17:30 +0100

    Add update and delete routes

--- blog/main.py
+++ blog/main.py
@@ -1,3 +1,5 @@
+from http.client import HTTPMessage
+
 from fastapi import FastAPI, Depends, status, HTTPException
 from dto.blogdto import BlogDTO
 from domain.blog import Blog
@@ -31,3 +33,23 @@ def show(id: int, db: Session = Depends(get_db)):
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
     return blog
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, request: BlogDTO, db: Session = Depends(get_db)):
+    blog = db.query(Blog).filter(Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
+    blog.update(request.dict(), synchronize_session=False)
+    db.commit()
+    return {"ok": True}
+
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(get_db)):
+    blog = db.query(Blog).filter(Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return {"ok": True}
