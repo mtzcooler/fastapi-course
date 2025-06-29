a2974001fb137f5f749a184f343f43d182e5d105
Matheus Zoccoler <mtzcooler@gmail.com>
2025-06-29 17:51:40 +0100

    Define status codes and exceptions

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, HTTPException
 from dto.blogdto import BlogDTO
 from domain.blog import Blog
 from database import engine, Base, get_db
@@ -10,7 +10,7 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: BlogDTO, db: Session = Depends(get_db)):
     new_blog = Blog(title=request.title, body=request.body, published=request.published)
     db.add(new_blog)
@@ -25,7 +25,9 @@ def index(db: Session = Depends(get_db)):
     return blogs
 
 
-@app.get('/blog/{id}')
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
 def show(id: int, db: Session = Depends(get_db)):
     blog = db.query(Blog).filter(Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
     return blog
